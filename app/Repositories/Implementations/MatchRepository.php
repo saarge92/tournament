53fdd985a91c056afb6ea3abee1a705003ee2e49
Serdar Durdyev <sarage92@mail.ru>
2020-12-20 10:21:27 +0400

    Update MatchRepository info comments

--- app/Repositories/Implementations/MatchRepository.php
+++ app/Repositories/Implementations/MatchRepository.php
@@ -13,17 +13,34 @@ use App\Repositories\Interfaces\IMatchRepository;
  */
 class MatchRepository implements IMatchRepository
 {
-
+    /**
+     * Создание матча в базе
+     * @param array $data Данные инициализации матча
+     * @return Match Вернем созданный матч
+     */
     function createMatch(array $data): Match
     {
         return Match::create($data);
     }
 
+    /**
+     * Получение матча по id
+     * @param int $id Id матча
+     * @return Match|null Вернем найденный матч
+     */
     function getMatch(int $id): ?Match
     {
         return Match::find($id);
     }
 
+    /**
+     * Получение матча по id турнира, id турнирного этапа и id команд
+     * @param int $teamHome Id команды хозяев
+     * @param int $teamGuest Id команды гостей
+     * @param int $idTournament Id турнира
+     * @param int $idStage Id этапа турнира
+     * @return Match|null Вернем информацию о матче или пустую запись
+     */
     function getMatchByTeamTournamentStage(int $teamHome, int $teamGuest, int $idTournament, int $idStage): ?Match
     {
         return Match::where([
@@ -34,6 +51,13 @@ class MatchRepository implements IMatchRepository
         ])->first();
     }
 
+    /**
+     * Получение данных матча одной команды, где она была либо хозяином либо гостем в матче
+     * @param int $idTeam Id команды
+     * @param int $tournamentId Id турнира
+     * @param int $stageId Id группового турнира
+     * @return mixed
+     */
     function getMatchesByTeamIdAndTournament(int $idTeam, int $tournamentId, int $stageId)
     {
         return Match::whereRaw("(id_team_home = ? OR id_team_guest = ?) AND (id_tournament = ? and id_stage =?)",
